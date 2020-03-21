f99245a526b26818fb2076fcbbe75343c3889a57
Adam Oleszko <adamoleszko44@gmail.com>
2020-03-21 06:38:22 +0100

    Display current score

--- src/Components/AppBar/AppBar.jsx
+++ src/Components/AppBar/AppBar.jsx
@@ -3,8 +3,11 @@ import { Image, Header, Segment, Icon } from 'semantic-ui-react';
 
 import sampleProfile from '../../assets/images/Profile picture.png';
 import { styles } from './styles';
+import {useAuthorization} from "../../hooks/useAuthorization";
 
 const AppBar = () => {
+    const user = useAuthorization()
+
   return (
     <div fixed="top" style={styles.root}>
       <div>
@@ -16,7 +19,7 @@ const AppBar = () => {
         <Segment style={styles.segment}>
           <Header size="small">
             <Icon name="star" color="yellow" size="small" />
-            <Header.Content>320</Header.Content>
+            <Header.Content>{user ? user.currentScore : ''}</Header.Content>
           </Header>
         </Segment>
       </span>
